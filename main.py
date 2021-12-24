0f033412da6d4483b99024fd9e7168d9b31443e6
Bulat <gareevbulich63@gmail.com>
2021-12-24 16:49:03 +0500

    commit mes

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('heloo')
+import pygame
+
+
+if __name__ == '__main__':
+    pygame.init()
+    width, height =  1000, 700
+    size = width, height
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('')
+    all_sprites = pygame.sprite.Group()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        all_sprites.draw(screen)
+        all_sprites.update()
+        pygame.display.flip()

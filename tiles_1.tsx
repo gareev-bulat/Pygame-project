0aa49f477ebb23ed0d3f81f78459da58b38a96f8
Bulat <gareevbulich63@gmail.com>
2022-01-14 23:32:52 +0500

    tiles

---
+++ tiles_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tiles_1" tilewidth="64" tileheight="64" tilecount="225" columns="15">
+ <image source="../../Downloads/tiles_1.jpg" width="970" height="970"/>
+</tileset>

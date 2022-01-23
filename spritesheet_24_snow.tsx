0d26054e7fb654acf04ef05e32047d367976de60
Bulat <gareevbulich63@gmail.com>
2022-01-24 00:00:33 +0500

    snow tiles

---
+++ spritesheet_24_snow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="spritesheet_24_snow" tilewidth="64" tileheight="64" tilecount="225" columns="15">
+ <image source="spritesheet_24_snow.png" width="970" height="970"/>
+</tileset>
